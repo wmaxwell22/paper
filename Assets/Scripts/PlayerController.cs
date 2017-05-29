9103b5cd7b1bbae6714ff5636d271fdcb18e51ea
Maxwell <max@whitney.net>
2017-05-29 02:11:32 -0500

    initial files committed!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float vmove = Input.GetAxisRaw ("Vertical");
+		float hmove = Input.GetAxisRaw ("Horizontal");
+
+		Vector3 movement = new Vector3 (hmove,vmove);
+
+		transform.Translate (movement*speed*Time.deltaTime);
+
+		
+	}
+}
